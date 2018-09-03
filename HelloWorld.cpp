84a496b6c12f104860a27af2d4d23032643bf081
Umut Tanir <umut.Tanir@yahoo.com>
2018-09-03 21:37:14 +0200

    Added a simple Hello World Application using CMake

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-  cout << "Hello World from BlackEagleCode Repository!" << endl;
+int main(int argc, char *argv[])
+{  
+
+  cout << "Hello World from BlackEagleCode GitHub Repository!\n";
+
   return 0;
-}  
+}
